a38ffdabab988317765200469904f03ca9d9e073
AditiPawar24 <aditi.pawar249@gmail.com>
2020-04-27 23:10:06 +0530

    FlipCoin

---
+++ flipUC2.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -x
+
+echo "Flip coin"
+
+echo "multiple time user want to flip a coin"
+read flip
+echo $flip
+
+countTT=0
+countTH=0
+countHH=0
+countHT=0
+declare -A noOfCounts
+for(( i=0; i<flip; i++ ))
+do
+if [ $(($RANDOM%4)) -eq 0 ]
+then
+((countHH++))
+elif [ $(($RANDOM%4)) -eq 1 ]
+then
+((countTT++))
+elif [ $(($RANDOM%4)) -eq 2 ]
+then
+((countTH++))
+else
+((countHT++))
+fi
+done
+
+echo "HH"$countHH
+echo "TT"$countTT
+echo "TH"$countTH
+echo "HT"$countHT
+
+noOfCounts[hh]=$countHH
+noOfCounts[tt]=$countTT
+noOfCounts[th]=$countTH
+noOfCounts[ht]=$countHT
+
+echo ${noOfCounts[@]}
+
+
+
+
